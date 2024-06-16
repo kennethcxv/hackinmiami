05f2c044210414c5855502713c55c2c57f619bde
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-06-15 20:50:51 -0400

    did new backend

---
+++ app/api/gemini/take-2/route.js
@@ -0,0 +1,98 @@
+import { NextResponse } from "next/server";
+
+const { VertexAI } = require("@google-cloud/vertexai");
+
+export async function POST(req, res) {
+  const { symptoms, location } = await req.json();
+
+  // Initialize Vertex with your Cloud project and location
+  const vertex_ai = new VertexAI({
+    project: "miami-ai-hack24mia-911",
+    location: "us-central1",
+  });
+  const model = "gemini-1.5-flash-001";
+
+  const textsi_1 =
+    "You are a health advisor AI. Your role is to identify potential diseases based on a list of symptoms provided by the user, suggest necessary diagnostic tests, recommend nearby hospitals based on the zip code provided by the user, and provide cost estimates for those diagnostics. Your response should be empathetic and professional. Ensure to return the results in a JSON format with the key being the disease and the value being a list of the diagnostic tests, recommended hospitals, and their associated costs.";
+  const textsi_2 =
+    "Return the results in a JSON format where the key is the name of the category and the value is a list containing values for those category, I have define the category below, I only need that nothing else, and try to do google search for the hospitals based on the zip code";
+
+  // Instantiate the models
+  const generativeModel = vertex_ai.preview.getGenerativeModel({
+    model: model,
+    generationConfig: {
+      maxOutputTokens: 8192,
+      temperature: 0.5,
+      topP: 0.95,
+    },
+    safetySettings: [
+      {
+        category: "HARM_CATEGORY_HATE_SPEECH",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE",
+      },
+      {
+        category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE",
+      },
+      {
+        category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE",
+      },
+      {
+        category: "HARM_CATEGORY_HARASSMENT",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE",
+      },
+    ],
+    tools: [
+      {
+        googleSearchRetrieval: {
+          disableAttribution: false,
+        },
+      },
+    ],
+    systemInstruction: {
+      parts: [
+        {text: textsi_1},
+        { text: "Instructions:" },
+        { text: "Analyze the given symptoms to identify potential diseases." },
+        {
+          text: "For each potential disease, suggest the necessary diagnostic tests.",
+        },
+        {
+          text: "Provide a list of nearby hospitals that can perform these tests.",
+        },
+        {
+          text: "Include estimated costs for these tests at the specified hospitals.",
+        },
+        { text: "Possible diseases" },
+        { text: "The necessary diagnostic tests." },
+        { text: "Recommended hospitals." },
+        { text: "Estimated costs at those hospitals." },
+        {
+          text: 'Example input\n\n{"Symptoms": ["symptom1","symptom2"], "location": 33174}',
+        },
+        {text: textsi_2}
+      ],
+    },
+  });
+
+  const chat = generativeModel.startChat({});
+
+  const message = [
+    {
+      text: `{"Symptoms": [${symptoms.map((s) => `"${s}"`).join(', ')}], "location": ${location}}`,
+    },
+  ];
+//   const message = [
+//     {
+//       text: `{"Symptoms": [${["diarrhea, vomiting"].map((s) => `"${s}"`).join(', ')}], "location": 33174}`,
+//     },
+//   ];
+
+  const streamResult = await chat.sendMessageStream(message);
+  const response = await streamResult.response;
+
+const object = JSON.parse(response.candidates[0].content.parts[0].text)
+
+  return NextResponse.json({ data: object }, { status: 200 });
+}
--- app/page.js
+++ app/page.js
@@ -27,12 +27,13 @@ export default function Home() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("/api/gemini", {
+    const response = await fetch("/api/gemini/take-2", {
       method: "POST",
-      body: JSON.stringify({ userInput: symptomList.join(", "), zipCode }),
+      body: JSON.stringify({ symptoms: symptomList, location: zipCode }),
     });
     const { data } = await response.json();
-    setEstimatedCost(data);
+    console.log(data)
+    // setEstimatedCost(data);
   };
 
   return (
@@ -46,106 +47,100 @@ export default function Home() {
       </Head>
 
       <main className="container mx-auto py-12 px-4">
-    <div className="flex justify-center mb-8">
-      <Image src="/logo.png" alt="MedCost Logo" width={200} height={200} />
-    </div>
-    <h1 className="text-4xl font-bold mb-2 text-center text-white">
-      MedCost
-    </h1>
-    <p className="text-xl mb-8 text-center text-white">
-      Know Your Health, Plan Your Costs
-    </p>
-    <form onSubmit={handleSubmit} className="mb-8">
-      <div className="relative mb-4">
-        <input
-          id="symptoms"
-          type="text" 
-          placeholder="Describe your symptoms"
-          className="w-full p-4 text-lg text-white border-4 bg-transparent border-blue-800 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500 pl-8"
-          value={symptoms}
-          onChange={(e) => setSymptoms(e.target.value)}
-          onKeyPress={handleKeyPress}
-        />
-        <div className="absolute inset-y-0 right-0 flex items-center pr-6">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-8 w-8 text-blue-500 bg-blue-800 rounded-full p-1"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
+        <div className="flex justify-center mb-8">
+          <Image src="/logo.png" alt="MedCost Logo" width={200} height={200} />
         </div>
-      </div>
-      <div className="flex flex-wrap mb-4">
-        {symptomList.map((symptom, index) => (
-          <span
-            key={index}
-            className="inline-flex items-center bg-blue-100 text-blue-800 px-3 py-1 rounded-full mr-2 mb-2 cursor-pointer hover:bg-red-500 hover:text-white"
-            onClick={() => removeSymptom(index)}
-          >
-            {symptom}
-            <span className="ml-2 text-red-500 font-bold hover:text-white">
-              &#10005;
-            </span>
-          </span>
-        ))}
-      </div>
-      <div className="relative mb-4">
-        <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold mb-2 text-center text-white">
+          MedCost
+        </h1>
+        <p className="text-xl mb-8 text-center text-white">
+          Know Your Health, Plan Your Costs
+        </p>
+        <form onSubmit={handleSubmit} className="mb-8">
+          <div className="relative mb-4">
+            <input
+              id="symptoms"
+              type="text"
+              placeholder="Describe your symptoms"
+              className="w-full p-4 text-lg text-white border-4 bg-transparent border-blue-800 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500 pl-8"
+              value={symptoms}
+              onChange={(e) => setSymptoms(e.target.value)}
+              onKeyPress={handleKeyPress}
+            />
+            <div className="absolute inset-y-0 right-0 flex items-center pr-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 text-blue-500 bg-blue-800 rounded-full p-1"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+          <div className="flex flex-wrap mb-4">
+            {symptomList.map((symptom, index) => (
+              <span
+                key={index}
+                className="inline-flex items-center bg-blue-100 text-blue-800 px-3 py-1 rounded-full mr-2 mb-2 cursor-pointer hover:bg-red-500 hover:text-white"
+                onClick={() => removeSymptom(index)}
+              >
+                {symptom}
+                <span className="ml-2 text-red-500 font-bold hover:text-white">
+                  &#10005;
+                </span>
+              </span>
+            ))}
+          </div>
+          <div className="relative mb-4">
+            <div className="flex justify-between items-center">
+              <input
+                id="zipCode"
+                type="text"
+                placeholder="Zip Code"
+                className="w-1/4 p-4 text-lg text-white border-4 bg-transparent border-blue-800 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 pl-8"
+                value={zipCode}
+                onChange={(e) => setZipCode(e.target.value)}
+              ></input>
+              <div className="absolute inset-y-0 left-[16rem] flex items-center pr-6">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-8 w-8 text-blue-500 bg-blue-800 rounded-full p-1"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
 
-  <input
-    id="zipCode"
-    type="text"
-    placeholder="Zip Code"
-    className="w-1/4 p-4 text-lg text-white border-4 bg-transparent border-blue-800 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 pl-8"
-    value={zipCode}
-    onChange={(e) => setZipCode(e.target.value)}
-  >
-    
-  </input>
-  <div className="absolute inset-y-0 left-[16rem] flex items-center pr-6">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-8 w-8 text-blue-500 bg-blue-800 rounded-full p-1"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fillRule="evenodd"
-        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-        clipRule="evenodd"
-      />
-    </svg>
-  </div>
-</div>
-  </div>
-        
-  
-  
-      <div className="flex justify-start">
-        <button
-          type="submit"
-          className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-4 px-8 rounded-full text-xl"
-        >
-          Search
-        </button>
-      </div>
-    </form>
-    {estimatedCost && (
-      <div className="mt-8 bg-white p-6 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold mb-2 text-blue-900">
-          Estimated Hospital Cost
-        </h2>
-        <p className="text-blue-900 text-xl">{estimatedCost}</p>
-      </div>
-    )}
-  </main>
-</div>
-
+          <div className="flex justify-start">
+            <button
+              type="submit"
+              className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-4 px-8 rounded-full text-xl"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+        {estimatedCost && (
+          <div className="mt-8 bg-white p-6 rounded-lg shadow-lg">
+            <h2 className="text-2xl font-bold mb-2 text-blue-900">
+              Estimated Hospital Cost
+            </h2>
+            <p className="text-blue-900 text-xl">{estimatedCost}</p>
+          </div>
+        )}
+      </main>
+    </div>
   );
 }
